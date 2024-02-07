5f7a40800dd36d79dc7765c217019736e8aca701
Martin <mmaina290@gmail.com>
2024-02-07 18:25:39 +0300

    manual migrations

---
+++ alembic/versions/fdd9fc04ea55_renaming_students_to_scholars.py
@@ -0,0 +1,24 @@
+"""Renaming students to scholars
+
+Revision ID: fdd9fc04ea55
+Revises: b00948ad81c8
+Create Date: 2024-02-07 18:24:34.790795
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'fdd9fc04ea55'
+down_revision = 'b00948ad81c8'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
